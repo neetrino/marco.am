9932397663a7e27514b4100d1dac0045305ee81c
Liana <lianaengoyan04@gmail.com>
2026-04-07 17:42:51 +0400

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { HeroCarousel } from '../components/HeroCarousel';
-import { HomePromoSection } from '../components/home/HomePromoSection';
+
 import { FeaturesSection } from '../components/FeaturesSection';
 import { TopCategories } from '../components/TopCategories';
 import { FeaturedProductsTabs } from '../components/FeaturedProductsTabs';
@@ -9,9 +9,6 @@ export default async function HomePage() {
   return (
     <div className="min-h-screen">
       <HeroCarousel />
-      <section>
-        <HomePromoSection />
-      </section>
 
       {/* Top Categories */}
       <TopCategories />
